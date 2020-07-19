e26a09ea2e81a5df5209567a3c627e9a497bbf36
Myckhel <myckhel1@hotmail.com>
2020-07-19 15:44:22 +0100

    Updates

---
+++ static/js/15.c7e8a035.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpgfxclient=this.webpackJsonpgfxclient||[]).push([[15],{482:function(e,a,t){"use strict";t.r(a);t(100),t(63),t(58),t(102);var n=t(73),s=t(0),c=t.n(s);t(76);a.default=function(){var e=Object(s.useState)({loading:!1,name:"",password:""}),a=Object(n.a)(e,2),t=a[0];a[1],t.loading,t.name,t.password;return c.a.createElement("div",{className:"app flex-row align-items-center"})}}}]);
+//# sourceMappingURL=15.c7e8a035.chunk.js.map
